896ede83044f0c1bb288deb484581fe4a6aa48f8
arunp007 <arunp4624@gmail.com>
2022-07-15 16:48:45 +0530

    Version 1

---
+++ pattuproject/pattuapp/views.py
@@ -0,0 +1,155 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'home.html')
+
+def home2(request):
+    return render(request, 'home2.html')
+
+def home3(request):
+    return render(request, 'home3.html')
+
+def vikram(request):
+    return render(request, 'vikram.html')
+
+def kgf2(request):
+    return render(request, 'kgf2.html')
+
+def kgf2t(request):
+    return render(request, 'kgf2t.html')
+
+def beast(request):
+    return render(request, 'beast.html')
+
+def hridayam(request):
+    return render(request, 'hridayam.html')
+
+def janaganamana(request):
+    return render(request, 'janaganamana.html')
+
+def beeshma(request):
+    return render(request, 'beeshma.html')
+
+def sooryavanshi(request):
+    return render(request, 'sooryavanshi.html')
+
+def dilbechara(request):
+    return render(request, 'dilbechara.html')
+
+def major(request):
+    return render(request, 'major.html')
+
+def minnal_murali(request):
+    return render(request, 'minnal_murali.html')
+
+def kurup(request):
+    return render(request, 'kurup.html')
+
+def malik(request):
+    return render(request, 'malik.html')
+
+def drishyam(request):
+    return render(request, 'drishyam.html')
+
+def yuvam(request):
+    return render(request, 'yuvam.html')
+
+def hey_sinamika(request):
+    return render(request, 'hey_sinamika.html')
+
+def soorarai(request):
+    return render(request, 'soorarai.html')
+
+def doctor(request):
+    return render(request, 'doctor.html')
+
+def may(request):
+    return render(request, 'may_maadham.html')
+
+def ninesix(request):
+    return render(request, '96.html')
+
+def pyaar(request):
+    return render(request, 'pyaar.html')
+
+def meri(request):
+    return render(request, 'meri_pyaari_bindu.html')
+
+def dil_se(request):
+    return render(request, 'dil_se.html')
+
+def swades(request):
+    return render(request, 'swades.html')
+
+def thodisi(request):
+    return render(request, 'thodisi.html')
+
+def criminal(request):
+    return render(request, 'criminal.html')
+
+def silsila(request):
+    return render(request, 'silsila.html')
+
+def kumbalangi(request):
+    return render(request, 'kumbalangi.html')
+
+def premam(request):
+    return render(request, 'premam.html')
+
+def made_in_usa(request):
+    return render(request, 'made_in_usa.html')
+
+def chandrolsavam(request):
+    return render(request, 'chandrolsavam.html')
+
+def kohinoor(request):
+    return render(request, 'kohinoor.html')
+
+def kathodu(request):
+    return render(request, 'kathodu.html')
+
+def mazha(request):
+    return render(request, 'mazha.html')
+
+def ayal(request):
+    return render(request, 'ayal.html')
+
+def sanah(request):
+    return render(request, 'sanah.html')
+
+def indian(request):
+    return render(request, 'indian.html')
+
+def ghajini(request):
+    return render(request, 'ghajini.html')
+
+def ayan(request):
+    return render(request, 'ayan.html')
+
+def pithamagan(request):
+    return render(request, 'pithamagan.html')
+
+def sillunu(request):
+    return render(request, 'sillunu.html')
+
+def vip(request):
+    return render(request, 'vip.html')
+
+def malayalam(request):
+    return render(request, 'malayalam.html')
+
+def malayalam2(request):
+    return render(request, 'malayalam2.html')
+
+def tamil(request):
+    return render(request, 'tamil.html')
+
+def tamil2(request):
+    return render(request, 'tamil2.html')
+
+def hindi(request):
+    return render(request, 'hindi.html')
+
+def trending(request):
+    return render(request, 'trending.html')
