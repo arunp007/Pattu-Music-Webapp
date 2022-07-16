33b22b58157c353f406d807f164a198d34432813
arunp007 <arunp4624@gmail.com>
2022-07-16 18:50:29 +0530

    Version 1

---
+++ pattuproject/pattuapp/static/javascript/search.js
@@ -0,0 +1,213 @@
+function search(){
+    var search = document.getElementById('text')
+
+    if(search.value == 'vikram' || search.value == 'Vikram'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/vikram")
+        return true
+    }
+
+    if(search.value == 'kgf chapter 2 malayalam' || search.value == 'KGF Chapter 2'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/kgf2")
+        return true
+    }
+
+    if(search.value == 'beast' || search.value == 'Beast'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/beast")
+        return true
+    }
+
+    if(search.value == 'hridayam' || search.value == 'Hridayam'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/hridayam")
+        return true
+    }
+
+    if(search.value == 'jana gana mana' || search.value == 'Jana gana mana' || search.value == 'Jana Gana Mana'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/janaganamana")
+        return true
+    }
+
+    if(search.value == 'beeshma parvam' || search.value == 'Beeshma Parvam'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/beeshma")
+        return true
+    }
+
+    if(search.value == 'major' || search.value == 'Major'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/major")
+        return true
+    }
+
+    if(search.value == 'sooryavanshi' || search.value == 'Sooryavanshi'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/sooryavanshi")
+        return true
+    }
+
+    if(search.value == 'dil bechara' || search.value == 'Dil Bechara'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/dilbechara")
+        return true
+    }
+
+    if(search.value == 'hey sinamika' || search.value == 'Hey Sinamika'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/hey_sinamika")
+        return true
+    }
+
+    if(search.value == 'soorarai pottru' || search.value == 'Soorarai Pottru'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/soorarai")
+        return true
+    }
+
+    if(search.value == 'doctor' || search.value == 'Doctor'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/doctor")
+        return true
+    }
+
+    if(search.value == 'minnal murali' || search.value == 'Minnal Murali'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/minnal_murali")
+        return true
+    }
+
+    if(search.value == 'kurup' || search.value == 'Kurup'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/kurup")
+        return true
+    }
+
+    if(search.value == 'drishyam' || search.value == 'Drishyam'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/drishyam")
+        return true
+    }
+
+    if(search.value == 'meri pyaari bindu' || search.value == 'Meri Pyaari Bindu'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/meri")
+        return true
+    }
+
+    if(search.value == 'dil se' || search.value == 'Dil Se'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/dil_se")
+        return true
+    }
+
+    if(search.value == 'swades' || search.value == 'Swades'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/swades")
+        return true
+    }
+
+    if(search.value == 'may maadham' || search.value == 'May Maadham'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/may")
+        return true
+    }
+
+    if(search.value == '96'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/96")
+        return true
+    }
+
+    if(search.value == 'pyaar prema kaadhal' || search.value == 'Pyaar Prema Kaadhal'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/pyaar")
+        return true
+    }
+
+    if(search.value == 'malik' || search.value == 'Malik'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/malik")
+        return true
+    }
+
+    if(search.value == 'yuvam' || search.value == 'Yuvam'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/yuvam")
+        return true
+    }
+
+    if(search.value == 'thodisi bewafaii' || search.value == 'Thodisi Bewafaii'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/thodisi")
+        return true
+    }
+
+    if(search.value == 'criminal' || search.value == 'Criminal'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/criminal")
+        return true
+    }
+
+    if(search.value == 'silsila' || search.value == 'Silsila'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/silsila")
+        return true
+    }
+
+    if(search.value == 'kumbalangi knights' || search.value == 'Kumbalangi Knights'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/kumbalangi")
+        return true
+    }
+
+    if(search.value == 'premam' || search.value == 'Premam'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/premam")
+        return true
+    }
+
+    if(search.value == 'made in usa' || search.value == 'Made In Usa' || search.value == 'Made In USA'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/made_in_usa")
+        return true
+    }
+
+    if(search.value == 'chandrolsavam' || search.value == 'Chandrolsavam'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/chandrolsavam")
+        return true
+    }
+
+    if(search.value == 'kohinoor' || search.value == 'Kohinoor'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/kohinoor")
+        return true
+    }
+
+    if(search.value == 'kathodu kathoram' || search.value == 'Kathodu Kathoram'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/kathodu")
+        return true
+    }
+
+    if(search.value == 'mazhathullikkilukkam' || search.value == 'Mazhathullikkilukkam'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/mazha")
+        return true
+    }
+
+    if(search.value == 'ayal kadha ezhuthukayanu' || search.value == 'Ayal Kadha Ezhuthukayanu'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/ayal")
+        return true
+    }
+
+    if(search.value == 'sanah moidutty' || search.value == 'Sanah Moidutty'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/sanah")
+        return true
+    }
+
+    if(search.value == 'indian' || search.value == 'Indian'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/indian")
+        return true
+    }
+
+    if(search.value == 'ghajini' || search.value == 'Ghajini'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/ghajini")
+        return true
+    }
+
+    if(search.value == 'ayan' || search.value == 'Ayan'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/ayan")
+        return true
+    }
+
+    if(search.value == 'pithamagan' || search.value == 'Pithamagan'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/pithamagan")
+        return true
+    }
+
+    if(search.value == 'sillunu oru kaadhal' || search.value == 'Sillunu Oru Kaadhal'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/sillunu")
+        return true
+    }
+
+    if(search.value == 'vip' || search.value == 'Velaiilla Pattadhari' || search.value == 'velaiilla pattadhari'){
+        window.open("https://pattu123.herokuapp.com/pattuapp/vip")
+        return true
+    }
+
+    else{
+        document.getElementById('search_error').innerHTML="** Please Enter A Valid Movie Name **"
+        return false
+    }
+}
